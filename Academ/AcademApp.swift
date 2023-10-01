3c421f66f8b7546a1df50325901ac06e13341f55
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-10-01 18:21:31 +0800

    Initial Commit

---
+++ Academ/AcademApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademApp.swift
+//  Academ
+//
+//  Created by T Krobot on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AcademApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
