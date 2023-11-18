81882512cb43bfa718acc7fd2e567d5935c43618
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-11-18 12:03:06 +0800

    added model

---
+++ Academ/Model.swift
@@ -0,0 +1,26 @@
+//
+//  Model.swift
+//  Academ
+//
+//  Created by T Krobot on 18/11/23.
+//
+
+import Foundation
+
+struct Assessment: Identifiable{
+    var id = UUID()
+    var name: String
+    var percentageValue: Float
+    var totalMarks: Float
+    var examDone: Bool
+    var markAttained: Float
+    var examDate: Date
+    var targetMarks: Float
+    var haveReminder: Bool
+    var reminder: Date
+}
+struct Subject: Identifiable{
+    var id = UUID()
+    var name: String
+    var assessments: [Assessment]
+}
