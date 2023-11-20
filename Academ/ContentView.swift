77ff1ec7136738106ad2fc0a27ed63e83d113715
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-11-20 10:06:01 +0800

    deleted stack

--- Academ/ContentView.swift
+++ Academ/ContentView.swift
@@ -10,28 +10,27 @@ import SwiftUI
 struct ContentView: View {
     @State private var selection = 2
     var body: some View {
-        VStack {
+        
+        
+        //                .offset(x:-70, y:-10)
+        
+        TabView(selection: $selection){
+            SubjectsView()
+                .tabItem{
+                    Label("Subjects", systemImage: "books.vertical")
+                        .ignoresSafeArea(.all)
+                }.tag(1)
+            DashboardView()
+                .tabItem{
+                    Label("Dashboard", systemImage: "house")
+                        .ignoresSafeArea(.all)
+                }.tag(2)
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                        .ignoresSafeArea(.all)
+                }.tag(3)
             
-            //                .offset(x:-70, y:-10)
-            
-            TabView(selection: $selection){
-                SubjectsView()
-                    .tabItem{
-                        Label("Subjects", systemImage: "books.vertical")
-                            .ignoresSafeArea(.all)
-                    }.tag(1)
-                DashboardView()
-                    .tabItem{
-                        Label("Dashboard", systemImage: "house")
-                            .ignoresSafeArea(.all)
-                    }.tag(2)
-                SettingsView()
-                    .tabItem {
-                        Label("Settings", systemImage: "gear")
-                            .ignoresSafeArea(.all)
-                    }.tag(3)
-                
-            }
         }
         
     }
