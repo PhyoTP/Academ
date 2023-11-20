5ad0c068ca1f55818f239ac61c83e1f16232567c
Ingo <ingocodingbecauseyes@gmail.com>
2023-11-20 10:04:18 +0800

    commit and push

--- Academ/Subject-stuff/SubjectDetailView.swift
+++ Academ/Subject-stuff/SubjectDetailView.swift
@@ -10,14 +10,23 @@ import SwiftUI
 struct SubjectDetailView: View {
     @Binding var sub: Subject
     var body: some View {
-        VStack{
-            
+        
+        NavigationStack {
+            VStack{
+                
+            }
+        }
+        .navigationTitle("Subjects")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                EditButton()
+            }
         }
     }
-}
-
-struct SubjectDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        SubjectDetailView(sub: .constant(Subject(name: "Mathematics", assessments: [])))
+    
+    struct SubjectDetailView_Previews: PreviewProvider {
+        static var previews: some View {
+            SubjectDetailView(sub: .constant(Subject(name: "Mathematics", assessments: [])))
+        }
     }
 }
