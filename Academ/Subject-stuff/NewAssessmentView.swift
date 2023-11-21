12702c9b36f98ff3a223f49a6a0b6327540eae4d
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-11-21 14:49:08 +0800

    changed to a premade assessment struct

--- Academ/Subject-stuff/NewAssessmentView.swift
+++ Academ/Subject-stuff/NewAssessmentView.swift
@@ -8,72 +8,59 @@
 import SwiftUI
 
 struct NewAssessmentView: View {
-    @State private var assessmentName = ""
-    @State private var assessmentPercentage = 0
-    @State private var totaledMarks = 0
-    //    @State private var targetedMarks = 0
-    @State private var examDoneness = false
-    @State private var marksAttain = 0
-    @State private var examsDate = Date()
-    @State private var hasReminder = false
-    @State private var reminderDate = Date()
+    @State private var newAssessment = Assessment(name: "", percentageValue: 0, totalMarks: 0, examDone: false, markAttained: 0, examDate: Date(), haveReminder: false, reminder: Date())
     @Environment(\.dismiss) var dismiss
     @Binding var sub: Subject
     var body: some View {
         Form{
             Section("Assessment Info"){
-                TextField("Name",text: $assessmentName)
+                TextField("Name",text: $newAssessment.name)
                 //TextField()
                 HStack{
                     Text("Percentage value:")
-                    TextField("Percentage", value: $assessmentPercentage, formatter: NumberFormatter())
+                    TextField("Percentage", value: $newAssessment.percentageValue, formatter: NumberFormatter())
                     Text("%")
                     
                     
                 }
                 HStack{
                     Text("Total marks:")
-                    TextField("Marks", value: $totaledMarks, formatter: NumberFormatter())
+                    TextField("Marks", value: $newAssessment.totalMarks, formatter: NumberFormatter())
                     
                 }
                 
                 HStack{
                     Text("Exam done?")
-                    Toggle(isOn: $examDoneness){
+                    Toggle(isOn: $newAssessment.examDone){
                         Text("")
                     }
                     
                 }
-                if examDoneness {
+                if newAssessment.examDone {
                     HStack{
-                        Text("Marks attaineed:")
-                        TextField("Marks", value: $marksAttain, formatter: NumberFormatter())
+                        Text("Marks attained:")
+                        TextField("Marks", value: $newAssessment.markAttained, formatter: NumberFormatter())
                     }
                 } else{
-                    //                    HStack{
-                    //                        Text("Target marks:")
-                    //                        TextField("Marks", value: $targetedMarks, formatter: NumberFormatter())
-                    //                    }
                     DatePicker(
                         "Exam Date",
-                        selection: $examsDate,
+                        selection: $newAssessment.examDate,
                         displayedComponents: [.date]
                     )
                     HStack{
                         Text("Have reminder?")
-                        Toggle(isOn: $hasReminder){
+                        Toggle(isOn: $newAssessment.haveReminder){
                             Text("")
                         }
                         
                     }
-                    if hasReminder{
-                        DatePicker("Reminder Date",selection: $reminderDate, displayedComponents: [.date])
+                    if newAssessment.haveReminder{
+                        DatePicker("Reminder Date",selection: $newAssessment.reminder, displayedComponents: [.date])
                     }
                 }
             }
             Section {
                 Button("Save", role: .none) {
-                    let newAssessment = Assessment(name: assessmentName, percentageValue: Float(assessmentPercentage), totalMarks: Float(totaledMarks), examDone: examDoneness, markAttained: examDoneness ? Float(marksAttain) : 0, examDate: examDoneness ? Date() : examsDate,  haveReminder: hasReminder, reminder: hasReminder ? reminderDate : Date())
                     sub.assessments.append(newAssessment)
                     dismiss()
                 }
