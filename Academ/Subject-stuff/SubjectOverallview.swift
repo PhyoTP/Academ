3be91041c8ce117b5dc4e064017810aaa9d8ae21
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-11-21 12:36:24 +0800

    coomitor

--- Academ/Subject-stuff/SubjectOverallview.swift
+++ Academ/Subject-stuff/SubjectOverallview.swift
@@ -24,6 +24,7 @@ struct SubjectOverallView: View {
                 }
                 HStack{
                     Text("Highest:")
+                    
                 }
             }
             .navigationTitle(subje.name)
