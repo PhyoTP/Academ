7894dc92e0b14c495a2cbc500332b3aefa13a424
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-11-24 10:04:36 +0800

    shows user what marks they need to get

--- Academ/Subject-stuff/SubjectOverallView.swift
+++ Academ/Subject-stuff/SubjectOverallView.swift
@@ -14,40 +14,49 @@ struct SubjectOverallView: View {
     var body: some View {
         NavigationStack{
             Form{
-                HStack{
-                    Text("Current Overall:")
-                    Text("\(String(format: "%.0f",subje.currentOverall()))")
-                    Text("%")
-                }//current overall
-                HStack{
-                    Text("Highest:")
-                    Text("\(Int(subje.highest()))")
-                    Text("%")
-                }//highest
-                HStack{
-                    Text("Overall Goal:")
-                    Text("\(Int(subje.targetGrade))%")
-                }//overall goal
-                if subje.assessments.count == subje.numOfAssessments{
+                Section{
                     HStack{
-                        Text("Goal achieved?")
-                        if subje.currentOverall() >= subje.targetGrade {
-                            Text("✅")
-                        } else {
-                            Text("❌")
+                        Text("Current Overall:")
+                        Text("\(String(format: "%.0f",subje.currentOverall()))")
+                        Text("%")
+                    }//current overall
+                    HStack{
+                        Text("Highest:")
+                        Text("\(Int(subje.highest()))")
+                        Text("%")
+                    }//highest
+                    HStack{
+                        Text("Overall Goal:")
+                        Text("\(Int(subje.targetGrade))%")
+                    }//overall goal
+                }
+                Section{
+                    if subje.assessmentArray(type: 1).count == subje.numOfAssessments{
+                        HStack{
+                            Text("Goal achieved?")
+                            if subje.currentOverall() >= subje.targetGrade {
+                                Text("✅")
+                            } else {
+                                Text("❌")
+                            }
+                        }//goal achieved
+                        if subje.currentOverall() == subje.targetGrade {
+                            Text("Goal already achieved. Well done! 😁")
+                        }//goal message
+                    }else {
+                        HStack{
+                            Text("Percentage needed to achieve goal:")
+                            Spacer()
+                            Text("\(Int(subje.weightedGoal())) %")
                         }
-                    }//goal achieved
-                    if subje.currentOverall() >= subje.targetGrade {
-                        Text("Goal already achieved. Well done! 😁")
-                    }//goal message
-                }else if subje.assessments.count >= subje.numOfAssessments{
-                    HStack{
-                        Text("You have more assessments than you input in 'No. of assessments. Please reduce the input or remove some assessments.")
-                    }
-                }else {
-                    HStack{
-                        Text("Percentage needed to get goal marks:")
-                        Text("\(Int(subje.weightedGoal())) %")
+                        List(subje.getUnfinishedAssessments()){assessme in
+                            HStack{
+                                Text("\(assessme.name) target marks:")
+                                Spacer()
+                                Text(String(format:"%.1f",assessme.totalMarks*subje.weightedGoal()/100))
+                                Text("/\(Int(assessme.totalMarks))")
+                            }
+                        }
                     }
                 }
             }
@@ -61,7 +70,7 @@ struct SubjectOverallView: View {
 struct SubjectOverallView_Previews: PreviewProvider {
     static var previews: some View {
         
-        SubjectOverallView(subje: .constant(Subject(name: "Mathematics", assessments: [Assessment(name: "WA1", percentageValue: 10, totalMarks: 20, examDone: true, markAttained: 12, examDate: Date(),  haveReminder: false, reminder: Date()),Assessment(name: "WA2", percentageValue: 15, totalMarks: 30, examDone: true, markAttained: 23, examDate: Date(),  haveReminder: false, reminder: Date())],targetGrade: 75, credits: 0, numOfAssessments: 4)))
+        SubjectOverallView(subje: .constant(Subject(name: "Mathematics", assessments: [Assessment(name: "WA1", percentageValue: 10, totalMarks: 20, examDone: true, markAttained: 12, examDate: Date(),  haveReminder: false, reminder: Date()),Assessment(name: "WA2", percentageValue: 15, totalMarks: 30, examDone: true, markAttained: 23, examDate: Date(),  haveReminder: false, reminder: Date()),Assessment(name: "WA3", percentageValue: 15, totalMarks: 45, examDone: false, markAttained: 0, examDate: Date(), haveReminder: false, reminder: Date()),Assessment(name: "EYE", percentageValue: 60, totalMarks: 120, examDone: false, markAttained: 0, examDate: Date(), haveReminder: false, reminder: Date())],targetGrade: 75, credits: 0, numOfAssessments: 4)))
         
     }
 }
