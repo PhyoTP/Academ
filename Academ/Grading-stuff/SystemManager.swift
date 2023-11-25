b68f070b0433e85cd549a758d3fe59b3a5fd7d5b
Ingo <ingocodingbecauseyes@gmail.com>
2023-11-25 21:54:34 +0900

    added some grades that pls check

--- Academ/Grading-stuff/SystemManager.swift
+++ Academ/Grading-stuff/SystemManager.swift
@@ -10,10 +10,10 @@ import SwiftUI
 
 class SystemManager: ObservableObject {
     @Published var systems: [GradeSystem] = [
-        GradeSystem(type: 1, grades: [
+        GradeSystem(type: 1, grades: [ // GPA
             //user input here
         ]),
-        GradeSystem(type: 2, grades: [
+        GradeSystem(type: 2, grades: [ // MSG
             Grade(name: "A1", minMark: 75, maxMark: 100, gradePoint: 1.0),
             Grade(name: "A2", minMark: 70, maxMark: 74, gradePoint: 2.0),
             Grade(name: "B3", minMark: 65, maxMark: 69, gradePoint: 3.0),
@@ -24,7 +24,7 @@ class SystemManager: ObservableObject {
             Grade(name: "D8", minMark: 40, maxMark: 44, gradePoint: 8.0),
             Grade(name: "F9", minMark: 0, maxMark: 39, gradePoint: 9.0),
         ]),
-        GradeSystem(type: 3, grades: [
+        GradeSystem(type: 3, grades: [ // AL
             Grade(name: "AL1", minMark: 90, maxMark: 100, gradePoint: 1.0),
             Grade(name: "AL2", minMark: 85, maxMark: 89, gradePoint: 2.0),
             Grade(name: "AL3", minMark: 80, maxMark: 84, gradePoint: 3.0),
@@ -33,7 +33,25 @@ class SystemManager: ObservableObject {
             Grade(name: "AL6", minMark: 45, maxMark: 64, gradePoint: 6.0),
             Grade(name: "AL7", minMark: 20, maxMark: 44, gradePoint: 7.0),
             Grade(name: "AL8", minMark: 0, maxMark: 19, gradePoint: 8.0),
-        ])] {
+        ]),
+        GradeSystem(type:4, grades: [ // Overall grade
+            Grade(name: "AL1", minMark: 90, maxMark: 100, gradePoint: 1.0),
+            Grade(name: "AL2", minMark: 85, maxMark: 89, gradePoint: 2.0),
+            Grade(name: "AL3", minMark: 80, maxMark: 84, gradePoint: 3.0),
+            Grade(name: "AL4", minMark: 75, maxMark: 79, gradePoint: 4.0),
+            Grade(name: "AL5", minMark: 65, maxMark: 74, gradePoint: 5.0),
+            Grade(name: "AL6", minMark: 45, maxMark: 64, gradePoint: 6.0),
+            Grade(name: "AL7", minMark: 20, maxMark: 44, gradePoint: 7.0),
+            Grade(name: "AL8", minMark: 0, maxMark: 19, gradePoint: 8.0),
+        ]),
+        GradeSystem(type:5, grades: [ // N(T)/N(A) levels grades
+            Grade(name: "A", minMark: 70, maxMark: 100, gradePoint: 1.0),
+            Grade(name: "B", minMark: 65, maxMark: 69, gradePoint: 2.0),
+            Grade(name: "C", minMark: 60, maxMark: 64, gradePoint: 3.0),
+            Grade(name: "D", minMark: 50, maxMark: 59, gradePoint: 4.0),
+            Grade(name: "U", minMark: 0, maxMark: 49, gradePoint: 5.0),
+            Grade(name: "N", minMark: 0, maxMark: 0, gradePoint: 6.0), //idk how to define N
+            ])] {
             didSet {
                 save()
             }
