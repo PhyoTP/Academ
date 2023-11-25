24b24c1144904841d5ec4eaec8aff07243d205d4
Ingo <ingocodingbecauseyes@gmail.com>
2023-11-25 11:19:04 +0900

    using hind to get one notification request at a time

--- Academ/Subject-stuff/AssessmentDetailView.swift
+++ Academ/Subject-stuff/AssessmentDetailView.swift
@@ -23,26 +23,32 @@ struct AssessmentDetailView: View {
     }
 
     func scheduleNotification(at date: Date, body: String, title: String) {
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.body = body
-        
-        let calendar = Calendar.current
-        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
-        
-        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("Error scheduling notification: \(error.localizedDescription)")
-            } else {
-                print("Notification scheduled successfully")
-                scheduleNotification(at: Date().addingTimeInterval(60), body: "Your exam is on \(assess.examDate)", title: assess.name)
-            }
-        }
+       // Remove all pending notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+           UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+       UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+
+       let content = UNMutableNotificationContent()
+       content.title = title
+       content.body = body
+
+       let calendar = Calendar.current
+       let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+
+       let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+
+       let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+       UNUserNotificationCenter.current().add(request) { error in
+           if let error = error {
+               print("Error scheduling notification: \(error.localizedDescription)")
+           } else {
+               print("Notification scheduled successfully")
+           }
+       }
     }
+
+
     var body: some View {
         NavigationStack{
             List{
--- Academ/Subject-stuff/NewAssessmentView.swift
+++ Academ/Subject-stuff/NewAssessmentView.swift
@@ -26,26 +26,31 @@ struct NewAssessmentView: View {
     @State private var markCheck:Float = 0.0
     @State var NotificationSet =  true
     func scheduleNotification(at date: Date, body: String, title: String) {
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.body = body
+       // Remove all pending notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+           UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+       UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
 
-        let calendar = Calendar.current
-        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+       let content = UNMutableNotificationContent()
+       content.title = title
+       content.body = body
 
-        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+       let calendar = Calendar.current
+       let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
 
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+       let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
 
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("Error scheduling notification: \(error.localizedDescription)")
-            } else {
-                print("Notification scheduled successfully")
-                scheduleNotification(at: Date().addingTimeInterval(60), body: "Your exam is on \(newAssessment.examDate)", title: newAssessment.name)
-            }
-        }
+       let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+       UNUserNotificationCenter.current().add(request) { error in
+           if let error = error {
+               print("Error scheduling notification: \(error.localizedDescription)")
+           } else {
+               print("Notification scheduled successfully")
+           }
+       }
     }
+
     var body: some View {
         Form{
             Section("Assessment Info"){
